0567ef650b2880b4d69560ec32f55cfbd1d88b42
Asis Rai <asis.rai01@gmail.com>
2022-08-24 20:56:53 +0800

    passport

---
+++ Strategies/local-strategy.js
@@ -0,0 +1,50 @@
+const LocalStrategy = require("passport-local").Strategy;
+
+module.exports = (passport, bcrypt, knex,bodyParser) => {
+  //set up local strategy //video 1:20
+
+  passport.use(
+    "local-signup", //name of startegy
+    new LocalStrategy(
+      { passReqToCallback: true,usernameField: "email" }, //allows us to pass back the entire request to the callback 
+      async (req, email, password, done) => {
+        let user = await knex("users").where({ email }).first(); //The first() function gives the first element of the collection.
+        console.log("fname", req.body.firstName);
+        console.log("lname", req.body.lastName);
+        // console.log("test", req);
+        if (user) {
+          return done(null, false, { message: "User already exists" });
+        }
+        const hash = await bcrypt.hash(password, 10);
+        let newUser = {
+          firstName : req.body.firstName,
+          lastName: req.body.lastName,
+          email,
+          password: hash,
+        };
+        const id = await knex("users").insert(newUser).returning("id"); //insert username and hash pw and return id in db
+        newUser.id = id[0].id; //adds id on newUser
+        return done(null, newUser);
+      }
+    )
+  );
+
+  //login 1.52
+  passport.use(
+    "local-login", //name of startegy
+    new LocalStrategy(
+      { usernameField: "email" }, //dont need to add if using username to login
+      async (email, password, done) => {
+        const user = await knex("users").where({ email }).first();
+        if (!user) {
+          return done(null, false);
+        }
+        const result = bcrypt.compare(password, user.password); //compares user input password with hash password
+        if (result) {
+          return done(null, user);
+        }
+        return done(null, false);
+      }
+    )
+  );
+};
--- index.js
+++ index.js
@@ -1,38 +1,65 @@
+const fs = require("fs"); //to access crt and key file 
+const https = require("https"); //for https social login
+
 const express = require("express");
 const { engine } = require("express-handlebars");
-const cors = require('cors');
+const cors = require("cors");
 
 const knexFile = require("./knexfile").development;
 const knex = require("knex")(knexFile);
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 
 const EventsRouter = require("./Routers/EventsRouter");
 const EventsService = require("./Services/EventsService");
 const UsersRouter = require("./Routers/UsersRouter");
 const UsersService = require("./Services/UsersService");
 const EventService = require("./Services/EventsService");
-const port = 9000;
+require("dotenv").config();
+const port = 3000;
 
-const app = express()
-app.use(cors())
+const setupPassport = require("./passport");
+const AuthRouter = require("./Routers/passport-router");
+
+const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-
+app.use(flash());
 // app.use(flash());
 
-const userService = new UsersService(knex)
-app.use("/user", new UsersRouter(userService, express).router())
+app.use(
+    session({
+      secret: process.env.SECRET_KEY, //used to sign for sessions cookie
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
 
-const eventService = new EventService(knex)
-app.use("/event", new EventsRouter(eventService, express).router())
+setupPassport(app, bcrypt, passport, knex);
+app.use("/", new AuthRouter(express, passport).router());
+
+const userService = new UsersService(knex);
+app.use("/user", new UsersRouter(userService, express).router());
+
+const eventService = new EventService(knex);
+app.use("/event", new EventsRouter(eventService, express).router());
 
 // https.createServer(options, app).listen(port, () => {
 //     console.log(`Listening on port ${port}`);
 //   });s
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+const options = {
+  cert: fs.readFileSync("./localhost.crt"),
+  key: fs.readFileSync("./localhost.key"),
+};
+
+https
+  .createServer(options, app)
+  .listen(port, () => console.log(`Listening to port ${port}`));
