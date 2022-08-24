cbf16ead9079e3b92c307a11d80c9b87c9dfcd21
alex <alex92335126@gmail.com>
2022-08-25 00:07:45 +0800

    updated event service

--- Routers/EventsRouter.js
+++ Routers/EventsRouter.js
@@ -7,13 +7,15 @@ class EventsRouters {
     router() {
       let router = this.express.Router();
       router.get("/", this.getAll.bind(this));
-      router.get("/:id", this.getByUser.bind(this))
-      router.post("/", this.post.bind(this));
+      router.get("/:id", this.getEventParticipant.bind(this))
+      router.get("/:id", this.getUserEvent.bind(this))
+      router.post("/", this.postEvent.bind(this));
       router.put("/:id", this.put.bind(this));
       router.delete("/:id", this.delete.bind(this));
       return router;
     }
   
+  //list all events 
     async getAll(req, res) {
       // ==== auth checking =====
       // let user = req.auth.user;
@@ -24,6 +26,32 @@ class EventsRouters {
       } catch (error) {
         res.status(500).send(error)
       }
+    }
+  //list out all users from an event 
+    async getEventParticipant(req, res) {
+      let eventId = req.params.id
+      console.log(eventId)
+      try {
+        const eventParticipant = await this.eventsService.listEventParticipant(eventId)
+        res.json(eventParticipant)
+      } catch (error) {
+        res.status(500).send(error) 
+      }
+    }
+// list out all events from an user
+    async getUserEvent(req, res) {
+      let userId = req.params.id
+      console.log(userId)
+      try {
+        const allUsers = await this.eventsService.listAllUser(userId)
+        res.json(allUsers)
+      } catch (error) {
+        res.status(500).send(error) 
+      } 
+    }
+
+
+
       // return (
       //   this.eventsService
       //     .list(user)
@@ -34,55 +62,38 @@ class EventsRouters {
       //       res.status(500).json(err);
       //     })
       // );
-    }
-    getByUser(req, res) {
-      let userId = req.params.id
-
-    }
   
-    /** # Post Method   #
-  /*  ====================== */
-    // 2) Create a post method
-    post(req, res) {
-      console.log('HI post note router')
+// post add event 
+    async postEvent(req, res) {
       let user = req.auth.user;
-      return (
-        this.eventsService
-          .add(req.body.note, user)
-          // call the add method here and pass note and user
-          .then(() => {
-            // list the notes of the user
-            return this.eventsService.list(user);
-          })
-          .then((events) => {
-            res.json(events);
-          })
-          .catch((err) => {
-            res.status(500).json(err);
-          })
-      );
+      try{
+        const updateEvent =  await this.eventsService.addEvent(req.body.event, user)
+        res.json(updateEvent)
+      }  catch (error) {
+        res.status(500).send(error) 
+      }
     }
   
-    put(req, res) {
-      let id = req.params.id;
-      let note = req.body.note;
-      let user = req.auth.user;
-      console.log("id", id);
-      return (
-        this.eventsService
-          .update(id, note, user)
-          .then(() => {
-            return this.noteService.list(user);
-          })
-          .then((events) => {
+    // put(req, res) {
+    //   let id = req.params.id;
+    //   let note = req.body.note;
+    //   let user = req.auth.user;
+    //   console.log("id", id);
+    //   return (
+    //     this.eventsService
+    //       .update(id, note, user)
+    //       .then(() => {
+    //         return this.noteService.list(user);
+    //       })
+    //       .then((events) => {
   
-            res.json(events);
-          })
-          .catch((err) => {
-            res.status(500).json(err);
-          })
-      );
-    }
+    //         res.json(events);
+    //       })
+    //       .catch((err) => {
+    //         res.status(500).json(err);
+    //       })
+    //   );
+    // }
   
     delete(req, res) {
       let id = req.params.id;
