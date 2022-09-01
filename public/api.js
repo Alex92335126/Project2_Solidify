bcfeeb86e7104228f54300962e895db6a493bbe0
Asis Rai <asis.rai01@gmail.com>
2022-09-01 18:47:19 +0800

    improved readme page for merit

--- public/api.js
+++ public/api.js
@@ -1,23 +1,22 @@
-let eventList = document.getElementById("event-data")
-let eventBtn = document.getElementById("event-button")
-console.log(eventList)
+let eventList = document.getElementById("event-data");
+let eventBtn = document.getElementById("event-button");
+console.log(eventList);
 
 function showEvent() {
-    if (eventList.style.display == "none") {
-        eventList.style.display = "block";
-        eventBtn.innerHTML = "Hide"
-
-    } else {
-        eventList.style.display = "none";
-        eventBtn.innerHTML = "See Event"
-    }
+  if (eventList.style.display == "none") {
+    eventList.style.display = "block";
+    eventBtn.innerHTML = "Hide";
+  } else {
+    eventList.style.display = "none";
+    eventBtn.innerHTML = "See Event";
+  }
 }
 
-async function getData () {
-    let res = await axios.get("https://localhost:3000/event")
-    console.log(res.data)
-    for(item of res.data) {
-        eventList.innerHTML += `
+async function getData() {
+  let res = await axios.get("https://localhost:3000/event");
+  console.log(res.data);
+  for (item of res.data) {
+    eventList.innerHTML += `
             <div class="event-data">
                 <div class="text-capitalize">${item.event_name}</div>
                 <div>${item.description}</div>
@@ -25,7 +24,17 @@ async function getData () {
                 <div>${item.event_start}</div>
                 <hr>
             </div>
-        `
-    }
+        `;
+  }
 }
-getData()
+getData();
+
+function callDateTime() {
+  var currentDate = new Date().toDateString();
+  var currentTime = new Date().toLocaleTimeString();
+  document.getElementById("watch").innerHTML = `${currentDate}-${currentTime}`;
+}
+
+setInterval(function () {
+  callDateTime();
+}, 1000);
